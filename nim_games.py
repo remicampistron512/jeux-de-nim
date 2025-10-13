c2bdb76d44b86a1493b1a3c4992938c90ab7d9fc
Rémi Campistron <remi.campistron@fms-ea.com>
2025-10-13 14:28:17 +0200

    Ajout de la selection du joueur et d'un squelette de fonctions diverses

--- nim_games.py
+++ nim_games.py
@@ -4,3 +4,48 @@
 """
 Jeux  de Nim (variante simple et de Marienbad)
 """
+
+
+def display_matches_stacks(matches_stacks):
+    """
+    Affiche les allumettes
+    :param matches_stacks:
+    :return:
+    """
+    for stack in matches_stacks:
+        for i in range(0, stack):
+            print(" | ", end=" ")
+
+
+def choose_starting_player(players):
+    pass
+
+
+def start_game():
+    pass
+
+
+def init_game(num_players):
+    """
+    Fonction principale qui initialise le jeu: recupere le nom des joeurs, affiche les allumettes et commence la partie
+    :param num_players:
+    :return:
+    """
+    players = init_players_names(num_players)
+    choose_starting_player(players)
+    matches_stacks = [0]
+    matches_stacks[0] = 21
+    display_matches_stacks(matches_stacks)
+    start_game()
+
+
+def init_players_names(num_players):
+    players = []
+    for i in range(1, num_players + 1):
+        name = input(f"Entrez le nom du joueur {i} : ")
+        players.append({"id": i, "name": name})
+    return players
+
+
+if __name__ == '__main__':
+    init_game(2)
