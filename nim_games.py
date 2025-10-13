5fa23737578ae56f484b36b224c96bd236e3e687
Rémi Campistron <remi.campistron@gmail.com>
2025-10-13 18:47:59 +0200

    Programme semi-fonctionnel, reste la validation des inputs et améliorer l'affichage des allumettes

--- nim_games.py
+++ nim_games.py
@@ -26,15 +26,47 @@ def choose_starting_player(num_players):
         else:
             print (i, end=" )  ? ")
 
-    starting_player = int(input(":  "))
+    starting_player_id = int(input(":  "))
 
 
-    return starting_player
+    return starting_player_id
+
+def compute_turn(current_player_id, players, matches_stacks):
+    display_matches_stacks(matches_stacks)
+    print(f"c'est a {get_player_name_by_id(current_player_id,players)} de jouer")
+    num_matches_removed = int(input("Combien d'allumettes souhaitez retirez"))
+    matches_stacks[0] -= num_matches_removed
+
+def init_cycle(players,starting_player_id):
+    id_cycle = cycle(p["id"] for p in players)
+    # avancer jusqu'au joueur de départ
+    cur = next(id_cycle)
+    while cur != starting_player_id:
+        cur = next(id_cycle)
+    return id_cycle
+
+def next_player(id_cycle):
+    current_player_id = next(id_cycle)
+    print ("next player :" + str(current_player_id))
+    return current_player_id
+
+def next_turn(current_player_id, players, matches_stacks,id_cycle):
+    print ("next turn")
+    if matches_stacks[0] > 1:
+
+        current_player_id = next_player(id_cycle)
+        compute_turn(current_player_id, players, matches_stacks)
+        next_turn(current_player_id, players, matches_stacks, id_cycle)
+    else:
+        print (f"{get_player_name_by_id(current_player_id,players)} a gagné !!")
 
 
-def start_game(starting_player,players):
-    current_player_name  = get_player_name_by_id(starting_player, players)
-    print(f"c'est a {current_player_name} de jouer")
+
+def start_game(starting_player_id,players,matches_stacks):
+    compute_turn(starting_player_id, players, matches_stacks)
+    id_cycle = init_cycle(players,starting_player_id)
+    print ("starting player" + str(starting_player_id))
+    next_turn(starting_player_id, players, matches_stacks,id_cycle)
 
 
 def get_player_name_by_id(player_id, players):
@@ -56,24 +88,18 @@ def init_game(num_players):
     :return:
     """
     players = init_players_names(num_players)
-    starting_player = choose_starting_player(num_players)
+
+    starting_player_id = choose_starting_player(num_players)
 
     matches_stacks = [0]
     matches_stacks[0] = 21
-    display_matches_stacks(matches_stacks)
-
-    start_game(starting_player,players)
 
 
-    while matches_stacks[0] > 1:
+    start_game(starting_player_id,players,matches_stacks)
 
-        id_cycle = cycle(p["id"] for p in players)
-        current_player_id = next(id_cycle)
+    print("finish")
 
-        print ("current id" + str(current_player_id))
-        display_matches_stacks(matches_stacks)
-        print(f"c'est a {get_player_name_by_id(current_player_id,players)} de jouer")
-        matches_stacks[0] -= 1
+
 
 def init_players_names(num_players):
     players = []
